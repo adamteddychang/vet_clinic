4f70464ef7dc980f4d5fdf595f7eda0e6db3fa0a
adamteddychang <adamchang0725@gmail.com>
2022-01-12 13:29:35 +0100

    modified animals table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,35 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 ADD species varchar(100);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name varchar(100),
+    age int
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name varchar(100)
+);
+
+ALTER TABLE animals
+DROP COLUMN id;
+
+ALTER TABLE animals
+ADD id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id int;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owners_id int;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
